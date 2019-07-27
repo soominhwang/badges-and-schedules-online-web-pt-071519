02d4981e464ec80d03fa3027f0e23a4fe0abf95c
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 21:20:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,6 @@ def batch_badge_creator(array)
   new_array=[]
   array=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   array.each do |name|
-    return "Hello, my name is #{name}."
     new_array << "Hello, my name is #{name}."
   end
   new_array(name)
