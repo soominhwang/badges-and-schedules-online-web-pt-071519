fb5609b29d1365d935995814814f35db22d69dda
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 21:40:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_rooms(array)
   array.each_with_index do |name, index|
-    "Hello, #{name}! You'll be assigned to room #{index}!"
+    return "Hello, #{name}! You'll be assigned to room #{index}!"
   end
 end
 
