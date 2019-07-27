7977e1ef59acf2a30cd0c76b9dcf82f4a9689690
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 21:04:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 # Write your code here.
-array=[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+array = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 
 def badge_maker(array)
   array.each do |name|
